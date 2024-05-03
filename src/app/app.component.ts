4c296b0f087ad5ec8cbb6bd02fdc56c2a4c4398d
Ari <privatedari@gmail.com>
2024-05-03 18:03:10 +0200

    general components implemented
    Took 1 hour 21 minutes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AuthComponent} from "./auth/auth/auth.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, AuthComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo " src="./assets/logoemptywhite.svg" alt="logo">
+        {{title}}
+      </header>
+    </main>
+    <app-auth></app-auth>
+  `,
+
+
+
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Chat-Up';
+  loggedIn:boolean = false;
+
+  public setLoggedIn(loggedValue : boolean){
+    this.loggedIn = loggedValue;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./chat/home/home.component";
+import {AuthComponent} from "./auth/auth/auth.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    title: 'login'
+  },
+  {}
+];
